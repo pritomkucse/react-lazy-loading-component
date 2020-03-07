e75e3adab871a035ac5ed0b8d55a97238d01b0d2
Pritom K Mondal <pritomkucse@gmail.com>
2020-03-07 14:25:59 +0600

    Init

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    constructor () {
+        super();
+    }
+
+    render () {
+        return (
+            <div>
+                HOME
+            </div>
+        )
+    }
+}
+
+export default Home
